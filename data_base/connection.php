fc2f510ed92552f349ac594226d975e91250220b
Jheymison Simões dos Santos <47837256+jheymison-simoes@users.noreply.github.com>
2020-10-08 16:48:30 -0300

    Update connection.php

--- data_base/connection.php
+++ data_base/connection.php
@@ -5,7 +5,7 @@ date_default_timezone_set("America/Sao_Paulo");
 
 // $conecta_bd = new mysqli("127.0.0.1", "root" ,"" , "eventosiftm");
 
-$conecta_bd = new mysqli("192.168.1.2:3306", "remotealuno" ,"iftm@123" , "eventosiftm");
+$conecta_bd = new mysqli("192.168.1.2", "remotealuno:3306" ,"iftm@123" , "eventosiftm");
 
 if ($conecta_bd-> connect_error ) {
 	die('Erro na conexao :' . $conecta_bd-> connect_error);
